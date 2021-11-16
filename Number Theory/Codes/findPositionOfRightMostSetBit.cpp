b80dc456f6d72fe8146354617e6fa3d0bd80aa77
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-11-17 02:29:27 +0530

    Create findPositionOfRightMostSetBit.cpp

---
+++ Number Theory/Codes/findPositionOfRightMostSetBit.cpp
@@ -0,0 +1,26 @@
+// C++ program for Position
+// of rightmost set bit
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class gfg
+{
+
+public:
+unsigned int getFirstSetBitPos(int n)
+{
+	return log2(n & -n) + 1;
+}
+};
+
+// Driver code
+int main()
+{
+	gfg g;
+	int n = 12;
+	cout << g.getFirstSetBitPos(n);
+	return 0;
+}
+
+// This code is contributed by SoM15242
